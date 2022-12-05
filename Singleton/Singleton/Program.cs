b21fec6ad5fe8fc96ff9ca0d89f64c08b23a198f
Popee114 <yoda.kazak@gmail.com>
2022-12-05 21:39:34 +0700

    Add singleton

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -1,5 +1,42 @@
 ﻿
 
+(new Thread(() =>
+{
+    Computer comp = new Computer();
+    comp.Launch();
+    Console.WriteLine(comp.OS.Name);
+})).Start();
+
+Computer comp = new Computer();
+// у нас не получится изменить ОС, так как объект уже создан    
+comp.OS = OS.GetInstance();
+Console.WriteLine(comp.OS.Name);
 
 
-Console.WriteLine("Hello, World!");
+Console.ReadKey();
+
+class OS
+{
+    private static readonly Lazy<OS> lazy =
+        new Lazy<OS>(() => new OS());
+
+    public string Name { get; private set; }
+
+    private OS()
+    {
+        Name = Guid.NewGuid().ToString();
+    }
+
+    public static OS GetInstance()
+    {
+        return lazy.Value;
+    }
+}
+class Computer
+{
+    public OS OS { get; set; }
+    public void Launch()
+    {
+        OS = OS.GetInstance();
+    }
+}
